7fba42f5ea06ad7143ae9aca93a6367c802941ec
joel90 <eyuelkassahun3@gmail.com>
2024-05-29 13:42:49 +0300

    the student home started

--- frontend/src/components/payment/PaymentForm.jsx
+++ frontend/src/components/payment/PaymentForm.jsx
@@ -22,7 +22,7 @@ const PaymentForm = () => {
                     {/* Add other payment form fields here */}
                     <button
                         type="submit"
-                        className="w-full py-2 px-4 bg-blue-500 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        className="  w-full py-2 px-4 bg-blue-500 text-white rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     >
                         Pay Now
                     </button>
