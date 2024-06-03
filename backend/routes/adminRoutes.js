8e820659c7bd569e5b4a03c4ed455ece6ea3567b
joel90 <eyuelkassahun3@gmail.com>
2024-06-03 09:56:43 +0300

    the payment method in progress

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -5,7 +5,10 @@ const {
     getAllCompanies,
     getCompanyById,
     approveCompanyApplication,
-    rejectCompanyApplication
+    rejectCompanyApplication,
+    createPlan,
+    updatePlan,
+    getAllPlans
 } = require("../controllers/adminController")
 const { protect, admin } = require("../middleware/authMiddleware")
 const router = express.Router()
@@ -15,5 +18,14 @@ router.put("/company/application/:applicationId/reject", protect, admin, rejectC
 router.get('/companies/status/:status', protect, admin, filterCompaniesByStatus);
 router.get('/companies', protect, getAllCompanies);
 router.get('/companies/:companyId', protect, getCompanyById);
+//planes
+// Route to create a plan
+router.post('/plans',protect, admin, createPlan);
+
+// Route to update a plan
+router.put('/plans/:planId',protect, admin, updatePlan);
+
+// Route to get all plans
+router.get('/plans', protect, getAllPlans);
 
 module.exports = router;
--- backend/routes/studentRoutes.js
+++ backend/routes/studentRoutes.js
@@ -1,9 +1,11 @@
 const express = require("express")
-const { applyToCompany, applyForInternship } = require("../controllers/studentController")
+const { applyToCompany, applyForInternship, selectPlan, paymentCallback } = require("../controllers/studentController")
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router()
 //apply for company
+router.post('/selectplan', protect, selectPlan);
+router.get('/payment/callback', paymentCallback);
 router.post("/apply", protect, applyToCompany);
 // Apply for an internship
 router.post('/internships/apply/:internshipId', protect, applyForInternship);
