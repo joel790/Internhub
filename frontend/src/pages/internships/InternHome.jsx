be0de6e64267aacee098c66a138e24d165a28802
Bewket <bewket@example.com>
2024-06-04 14:39:54 +0300

    Application form updated

--- frontend/src/pages/internships/InternHome.jsx
+++ frontend/src/pages/internships/InternHome.jsx
@@ -1,6 +1,5 @@
 // import React from "react";
 import intern from "../../assets/explore.png";
-import SearchField from "../../components/internship/SearchField";
 
 import FeaturedInternships from "./FeaturedInternships";
 import FindInternships from "./FindInternships";
@@ -14,7 +13,7 @@ const InternHome = () => {
             Explore Internship <br />
             Opportunities
           </h1>
-          <p className="text-xl lg:text-xl mb-8 text-gray-500">
+          <p className="text-xl lg:text-xl text-gray-500">
             We have many companies in various industries that offer internship
             opportunities for university students and recent graduates to help
             them gain experience, secure jobs, and develop their careers.
@@ -28,7 +27,6 @@ const InternHome = () => {
           />
         </div>
       </div>
-      <SearchField />
       <FeaturedInternships />
       <FindInternships />
       <div className="container mx-auto px-4"></div>
